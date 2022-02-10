a3d943022f06563d8abc2d19805fc0d7ab645878
Vasyl <vasyaboychuk@gmail.com>
2022-02-10 18:27:48 +0200

    added timestream insert

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('run')
+  runCommand(): string {
+    return this.appService.runCommand();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,39 @@
+import {
+  TimestreamWriteClient,
+  WriteRecordsCommand,
+  WriteRecordsCommandInput,
+} from '@aws-sdk/client-timestream-write';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  runCommand(): string {
+    const index = 0;
+    const record = {
+      Dimensions: [{ Name: 'fleet', Value: 'TestingFleet' }],
+      MeasureName: `test-measure #${index}`,
+      MeasureValue: '12.34',
+      Time: Date.now().toString(),
+    };
+
+    const writeInput: WriteRecordsCommandInput = {
+      DatabaseName: 'sampleDB-single',
+      TableName: 'IoT',
+
+      Records: [record],
+    };
+
+    const writeClient = new TimestreamWriteClient({ region: 'eu-central-1' });
+    writeClient.send(new WriteRecordsCommand(writeInput)).then(
+      () => {
+        // this.logger.debug('write response', response);
+      },
+      (err) => console.error('write error:', err),
+    );
+    return 'finished';
   }
 }
